07bb7c1c869d28ab2dea973baef26579f2d16c5b
Jordan <jtate1166@gmail.com>
2022-01-09 18:28:35 -0500

    Add ability to fetch 15 images from the nasa apod api

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,29 @@
-// making sure jest and eslint are working properly
-function sum(a, b) {
-  return a + b;
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-button");
+let searchValue;
+
+const auth = "YZCfYA51pkjZGlYqTpli2XMFTVAxYh133LzrIECX";
+
+async function apodImages() {
+  const fetchedData = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${auth}&count=15&concept_tags=true'`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+  const data = await fetchedData.json();
+  console.log(data);
+  data.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src="${photo.url}"></img>
+    <p>${photo.title}</p>`;
+    gallery.appendChild(galleryImg);
+  });
 }
 
-module.exports = {
-  sum,
-};
+apodImages();
--- javascript/app.test.js
+++
@@ -1,7 +0,0 @@
-const { expect, test } = require("@jest/globals");
-
-const app = require("./app");
-
-test("add 1 + 2 to equal 3", () => {
-  expect(app.sum(1, 2)).toBe(3);
-});
