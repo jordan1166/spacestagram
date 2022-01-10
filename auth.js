8781836bb1bf535727ba3d82db1ee56ddace381a
Jordan <jtate1166@gmail.com>
2022-01-10 08:24:22 -0500

    Add ability to search for image by date

---
+++ auth.js
@@ -0,0 +1,8 @@
+// import { config } from "./node_modules/dotenv";
+// config();
+// const apiKey = process.env.API_KEY;
+const auth = {
+  API_KEY: "3HFdZezxMWwQcFt9rKdEFulxI0OZCqGB6NLcnOlb",
+};
+
+export default auth;
--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,28 @@
+import auth from "../auth.js";
+
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-button");
+const searchForm = document.querySelector(".search-form");
 let searchValue;
 
-const auth = "YZCfYA51pkjZGlYqTpli2XMFTVAxYh133LzrIECX";
+const authKey = auth.API_KEY;
+
+// Event listeners
+searchInput.addEventListener("input", updateInput);
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchImage(searchValue);
+});
+
+function updateInput(event) {
+  searchValue = event.target.value;
+  console.log(typeof searchValue);
+}
 
 async function apodImages() {
+  // Fetch data from NASA imag api
   const fetchedData = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=${auth}&count=15&concept_tags=true'`,
+    `https://api.nasa.gov/planetary/apod?api_key=${authKey}&count=15&concept_tags=false'`,
     {
       method: "GET",
       headers: {
@@ -15,8 +30,10 @@ async function apodImages() {
       },
     }
   );
+  // Parse response into JSON
   const data = await fetchedData.json();
   console.log(data);
+  // Display images on webpage
   data.forEach((photo) => {
     if (photo.media_type === "image") {
       const galleryImg = document.createElement("div");
@@ -26,6 +43,35 @@ async function apodImages() {
       gallery.appendChild(galleryImg);
     }
   });
+}
+
+async function searchImage(search) {
+  // Fetch data from NASA imag api
+  try {
+  } catch (error) {}
+  const fetchedData = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${authKey}&date=${search}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    }
+  );
+  // Parse response into JSON
+  const data = await fetchedData.json();
+  // If date is not within the correct range, display an error message
+  if (data.msg) {
+    alert(data.msg);
+  }
+  // Display image on webpage
+  if (data.media_type === "image") {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src="${data.url}"></img>
+    <p>${data.title}</p>`;
+    gallery.appendChild(galleryImg);
+  }
 }
 
 apodImages();
